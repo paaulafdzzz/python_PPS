b4190c548c3970a9f4f232206abdeba7b02512dd
paaulafdzzz <paulita.fdez2003@gmail.com>
2025-11-07 18:01:57 +0100

    funciones en py

---
+++ funciones.py
@@ -0,0 +1,6 @@
+def funcion():
+    global x
+    x = "Lucas"
+
+funcion()
+print("Hola, me llamo " + x)
---
+++ variables.py
@@ -0,0 +1,3 @@
+nombre = "Lucas"
+edad = 21
+print("Hola, me llamo " + nombre + " y tengo " + str(edad) + " años.")
