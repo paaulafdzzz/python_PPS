f0f55f5e902d68ad00ef2a2162b260e78e74d04e
paaulafdzzz <paulita.fdez2003@gmail.com>
2025-11-12 17:09:31 +0100

    control flujo

---
+++ control de flujo.py
@@ -0,0 +1,11 @@
+a = 5
+b = 6
+
+if a < b:
+    print(f"{a} es mayor que {b}")
+elif a < b:
+    print(f"{a} es menor que {b}")
+else:
+    print(f"{a} y {b} son iguales")
+
+#añadir f"lo que sea, si es una variable se mete dentro de estas llaves {}"
