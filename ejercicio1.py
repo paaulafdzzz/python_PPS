cc0aece3af360a2026772e7e9a58a22d1176feaa
paaulafdzzz <paulita.fdez2003@gmail.com>
2025-11-26 11:21:23 -0500

    cambios de ejercicio

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,4 @@
-año = int(input("Ingrese un año: "))
+@profile
 
 def es_bisiesto(año):
     if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
@@ -6,6 +6,7 @@ def es_bisiesto(año):
     else:
         return False
 
+año = int(input("Ingrese un año: "))
 
 if es_bisiesto(año):
     print(f"{año} es un año bisiesto.")
